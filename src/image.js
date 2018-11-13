524dbfb1914923fe1b32ddafe963bf614bd9deb1
coocssweb <1974740999@qq.com>
2018-11-13 22:11:12 +0800

    add image function

--- src/image.js
+++ src/image.js
@@ -3,3 +3,35 @@
  * for：一些图片操作
  * Created by 王佳欣 on 2018/11/4.
  */
+
+/**
+ * 加载图片
+ */
+const load = (imageUrl) => {
+
+};
+
+/**
+ * 批量下载
+ * @param imageList
+ */
+const batchLoad = (imageList) => {
+
+};
+
+// 图片裁剪
+const cut = (image, {width, height}) => {
+
+};
+
+// 图片压缩
+const compress = (image) => {
+
+};
+
+export default {
+    load,
+    batchLoad,
+    cut,
+    compress
+};
